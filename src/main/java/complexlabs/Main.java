e261c8cdcd0ef91bb81bde636a068c55e1fc4cb8
CHUHAISTER <92538081+CHUHAISTER@users.noreply.github.com>
2022-11-18 13:57:39 +0300

    labs 5-6 and logger

--- src/main/java/complexlabs/Main.java
+++ src/main/java/complexlabs/Main.java
@@ -2,16 +2,12 @@ package main.java.complexlabs;
 
 import main.java.complexlabs.menu.MainMenu;
 
-
-//    public static Logger  LOGI = LogManager.getLogger(LoggerInfo.class);
-
 /**
  * The method main that call main menu of the program
  */
 public class Main {
 
     public static void main(String[] args){
-        main.java.complexlabs.Database.getConnection();
         MainMenu mainMenu = new MainMenu();
         mainMenu.execute();
     }
